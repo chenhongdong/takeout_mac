9d047b8e386041fce70d6834d902c3ce036fa005
chenzuoxi <12252792+chenzuoxi@user.noreply.gitee.com>
2024-10-18 17:29:21 +0800

    long转string

--- src/main/java/com/zuoxi/takeout/config/WebMvcConfig.java
+++ src/main/java/com/zuoxi/takeout/config/WebMvcConfig.java
@@ -1,8 +1,13 @@
 package com.zuoxi.takeout.config;
 
+import com.zuoxi.takeout.common.JacksonObjectMapper;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
@@ -15,4 +20,43 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         registry.addResourceHandler("/backend/**").addResourceLocations("classpath:/backend/");
         registry.addResourceHandler("/front/**").addResourceLocations("classpath:/front/");
     }
+
+    /**
+     * 扩展mvc框架的消息转换器
+     * @param converters
+     */
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // 创建消息转换器对象
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+        // 设置对象转换器，底层使用Jackson将java对象转为json
+        messageConverter.setObjectMapper(new JacksonObjectMapper());
+        // 将上面的消息转换器对象追加到mvc框架的转换器集合中，并且放在最靠前的位置(索引是0)
+        converters.add(0, messageConverter);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
